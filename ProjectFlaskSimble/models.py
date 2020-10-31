8758e5100591d016891a9c8ffc4e5047c11b19ef
unknown <mateus.informatica44@gmal.com>
2020-10-30 22:46:28 -0300

    First pull this project

---
+++ ProjectFlaskSimble/models.py
@@ -0,0 +1,14 @@
+
+
+class Usuario:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
+
+class Jogo:
+    def __init__(self, nome, categoria, console, id=None):
+        self.id = id
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
