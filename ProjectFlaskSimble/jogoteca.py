10e470151349ba6bf1d7bedb82dc5a23fea90346
unknown <mateus.informatica44@gmail.com>
2020-11-01 23:58:01 -0300

    Finish CRUD. Starting insert pictures .jpg.

--- ProjectFlaskSimble/jogoteca.py
+++ ProjectFlaskSimble/jogoteca.py
@@ -36,6 +36,31 @@ def criar():
     console = request.form['console']
     jogos = Jogo(nome, categoria, console)
     jogo_dao.salvar(jogos)
+
+    arquivo = request.files['arquivo']
+    arquivo.save(f'uploads/{arquivo.filename}')
+    return redirect(url_for('index'))
+
+@app.route('/editar/<int:id>')
+def editar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima = url_for('editar', id=id)))
+    jogo = jogo_dao.busca_por_id(id)
+    return render_template('editar.html', titulo = 'Editar o Jogo.', jogo=jogo)
+
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogos = Jogo(nome, categoria, console, id=request.form['id'])
+    jogo_dao.salvar(jogos)
+    return redirect(url_for('index'))
+
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    jogo_dao.deletar(id)
+    flash('O jogo foi deletado com sucesso.')
     return redirect(url_for('index'))
 
 @app.route('/login')
